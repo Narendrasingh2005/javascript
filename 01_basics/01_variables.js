e49b119cd51bbd53392798afde8847cf1c3603a0
Narendra Singh <ns1039492@gmail.com>
2026-01-12 14:04:48 +0000

    variabled done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountNo = 2414545;
+let accountemail = "abs@gmail.com";
+var accpass = "1233444";
+acccity = "jaipur";
+
+accountemail = "jbhg@gmail.com";
+accpass = "5145687";
+
+// accountNo = 2;
+console.log(accountNo);
+console.table([accountemail,accpass]);
